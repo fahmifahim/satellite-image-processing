f2ca0a6d83e850d38e8432683bd46c8488167b6a
Dzulfahmi <fahmifahim@gmail.com>
2020-06-08 20:53:13 +0900

    Update openstreetmap-test.py

--- openstreetmap-test.py
+++ openstreetmap-test.py
@@ -5,3 +5,18 @@ from io import BytesIO
 from keras.preprocessing.image import load_img, img_to_array
 import matplotlib.pyplot as plt
 %matplotlib inline
+
+
+# VARIABLE
+## API Domain for Tellus
+URL_DOMAIN = "gisapi.tellusxdp.com"
+## API token
+BEARER_TOKEN = "xxx"  # put your Tellus Token
+
+## X,Y,Z coordinate for specific place you want to observe
+Z = 13
+X = 7276
+Y = 3225
+
+if BEARER_TOKEN == "":
+    print("APIトークンがセットされていません")
