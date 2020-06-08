2d6268bf65bb4b7477ed6ff90d65b4eda9bfb984
Dzulfahmi <fahmifahim@gmail.com>
2020-06-08 20:51:37 +0900

    Create openstreetmap-test.py

---
+++ openstreetmap-test.py
@@ -0,0 +1,7 @@
+import numpy as np
+from skimage import io, color, img_as_ubyte, filters
+import requests
+from io import BytesIO
+from keras.preprocessing.image import load_img, img_to_array
+import matplotlib.pyplot as plt
+%matplotlib inline
