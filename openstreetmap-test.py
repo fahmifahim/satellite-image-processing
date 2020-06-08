2c34b987bcb0ed28de9c0c73ef8a033184da9228
Dzulfahmi <fahmifahim@gmail.com>
2020-06-08 20:58:02 +0900

    Update openstreetmap-test.py

--- openstreetmap-test.py
+++ openstreetmap-test.py
@@ -20,3 +20,22 @@ Y = 3225
 
 if BEARER_TOKEN == "":
     print("API Token need to be specified")
+
+    
+def get_data(img_type, domain=URL_DOMAIN, z=Z, x=X, y=Y, query=""):
+    if query != "":
+        query = "?" + query
+
+    # Set the token at header for Authorization
+    res = requests.get("https://{}/{}/{}/{}/{}.png{}".format(URL_DOMAIN, img_type, z, x, y, query),
+                   headers={"Authorization": "Bearer " + BEARER_TOKEN})
+
+    # NumPy array 
+    img = img_to_array(load_img(BytesIO(res.content)))
+    return img.astype(np.uint8)
+
+img_osm = get_data("osm")
+
+
+print(img_osm.shape)
+io.imshow(img_osm)
