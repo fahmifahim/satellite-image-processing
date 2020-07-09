8a8783060b19d43e55371d95406f27ad1619b349
Dzulfahmi <fahmifahim@gmail.com>
2020-07-09 19:47:25 +0900

    Update asnaro1-test51.py

--- asnaro1-test51.py
+++ asnaro1-test51.py
@@ -1,221 +1,148 @@
-{
- "cells": [
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "from asnaro import *\n",
-    "\n",
-    "# Tokyo station: 35.6812362,139.7649361\n",
-    "a_scenes=get_ASNARO_scene(20.425278, 122.933611, 45.557222, 153.986389)\n",
-    "\n",
-    "a_scenes_length = len(a_scenes)\n",
-    "print(\"Scenes length : \", a_scenes_length)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#観察したい地域の緯度経度を指定する latitude & longitude \n",
-    "min_lat = 20.425278\n",
-    "min_lon = 122.933611\n",
-    "max_lat = 45.557222\n",
-    "max_lon = 153.986389\n",
-    "asnaro_scene = get_ASNARO_scene(min_lat, min_lon, max_lat, max_lon)\n",
-    "#Filter scene in order\n",
-    "asnaro_scenes = filter_by_date(asnaro_scene, \n",
-    "                               datetime(2014, 12, 7, tzinfo=timezone.utc),\n",
-    "                               datetime(2019, 12, 31, tzinfo=timezone.utc))\n",
-    "\n",
-    "print(\"Asnaro Scenes: \", len(asnaro_scenes))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "#Specify your observation areas here : \n",
-    "observation_areas = [\n",
-    "    {\n",
-    "        \"name\":\"東京駅\",\n",
-    "        \"lat\": 35.6812362,\n",
-    "        \"lon\": 139.7649361\n",
-    "    },\n",
-    "    {\n",
-    "        \"name\":\"新宿駅\",\n",
-    "        \"lat\": 35.689607,\n",
-    "        \"lon\": 139.700571\n",
-    "    },\n",
-    "    {\n",
-    "        \"name\":\"国会議事堂\",\n",
-    "        \"lat\": 35.675888,\n",
-    "        \"lon\": 139.7426693\n",
-    "    }\n",
-    "]\n",
-    "\n",
-    "#Classify scenes by calculating distances and sort the acquisitionDate\n",
-    "#Calculate the difference between lat&lot of the observation areas and the firstly defined area \n",
-    "classified_scenes = classify(observation_areas, asnaro_scenes)\n",
-    "\n",
-    "tokyostation_scenes = classified_scenes[0]\n",
-    "shinjukustation_scenes = classified_scenes[1]\n",
-    "kokkai_scenes = classified_scenes[2]\n",
-    "print(\"東京駅-ASNARO: \",len(tokyostation_scenes))\n",
-    "print(\"新宿駅-ASNARO:\",len(shinjukustation_scenes))\n",
-    "print(\"国会議事堂-ASNARO：\",len(kokkai_scenes))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# 以下東京駅表示にエラー。。。\n",
-    "\n",
-    "# Refer the Tile info from the below URL to get the specific zoom, xtile and ytile\n",
-    "# https://maps.gsi.go.jp/development/tileCoordCheck.html#18/35.68131/139.76678\n",
-    "\n",
-    "#zoom=17\n",
-    "#xtile=116419\n",
-    "#ytile=51613\n",
-    "#tokyostation_image = get_ASNARO_image(tokyostation_scenes[0][\"entityId\"], zoom, xtile, ytile)\n",
-    "#print(tokyostation_image)\n",
-    "#io.imshow(tokyostation_image)\n",
-    "#plt.imshow(tokyostation_image)\n",
-    "#plt.show()\n",
-    "\n",
-    "#number_scenes = len(tokyostation_scenes)\n",
-    "\n",
-    "# Print all images in specified Zoom range\n",
-    "#for zoom in range (17,18):\n",
-    "#    print(zoom)\n",
-    "#    print_image(tokyostation_scenes, number_scenes, zoom)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "for scene in kokkai_scenes:\n",
-    "    print(scene[\"acquisitionDate\"])\n",
-    "    \n",
-    "\n",
-    "#zoom=18\n",
-    "#xtile=232839 #東京駅周辺x-18\n",
-    "#ytile=103225 #東京駅周辺y-18\n",
-    "\n",
-    "zoom=18\n",
-    "imgIndex=8\n",
-    "xtile=232836 #国会議事堂 周辺？\n",
-    "ytile=103230 #国会議事堂 周辺？\n",
-    "\n",
-    "#zoom=18\n",
-    "#xtile=232829 #国会議事堂x-18\n",
-    "#ytile=103231 #国会議事堂y-18\n",
-    "\n",
-    "#zoom=17 \n",
-    "#xtile=116415 #国会議事堂x-17\n",
-    "#ytile=51615 #国会議事堂y-17\n",
-    "\n",
-    "asnaro_image = get_ASNARO_image(kokkai_scenes[imgIndex][\"entityId\"], zoom, xtile, ytile)\n",
-    "io.imshow(asnaro_image)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Get ASNARO image in series format\n",
-    "def get_ASNARO_series_image(scene_id, zoom, topleft_x, topleft_y, size_x=1, size_y=1):\n",
-    "    img = []\n",
-    "    for y in range(size_y):\n",
-    "        row = []\n",
-    "        for x in range(size_x):\n",
-    "            row.append(get_ASNARO_image(scene_id, zoom, topleft_x + x, topleft_y + y))\n",
-    "        img.append(np.hstack(row))\n",
-    "    return  np.vstack(img)\n",
-    "\n",
-    "zoom = 18\n",
-    "imgIndex=8\n",
-    "topleft_x = 232836 #国会議事堂 周辺？\n",
-    "topleft_y = 103230 #国会議事堂 周辺？\n",
-    "size_x = 10\n",
-    "size_y = 10\n",
-    "asnaro_series_image = get_ASNARO_series_image(kokkai_scenes[imgIndex][\"entityId\"], zoom, topleft_x, topleft_y, size_x, size_y)\n",
-    "io.imshow(asnaro_series_image)"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Get ASNARO image in grid display\n",
-    "def make_grid_image(images, col):\n",
-    "    img = []\n",
-    "    for i in range(math.ceil(len(images)/col)):\n",
-    "        row = []\n",
-    "        for j in range(col):\n",
-    "            index = i*col + j\n",
-    "            if index < len(images):\n",
-    "                row.append(images[index])\n",
-    "            else:\n",
-    "                row.append(np.ones_like(imgs[0])*255)\n",
-    "        img.append(np.hstack(row))\n",
-    "    return np.vstack(img)\n",
-    "\n",
-    "imgs = [get_ASNARO_series_image(scene[\"entityId\"], zoom, topleft_x, topleft_y, size_x, size_y) for scene in kokkai_scenes]\n",
-    "plt.rcParams[\"figure.figsize\"] = (20, 20)\n",
-    "io.imshow(make_grid_image(imgs, 5))"
-   ]
-  },
-  {
-   "cell_type": "code",
-   "execution_count": null,
-   "metadata": {},
-   "outputs": [],
-   "source": [
-    "# Zoom = 18 で表示できるxtileとytileを調べる\n",
-    "\n",
-    "number_scenes = len(kokkai_scenes)\n",
-    "print(number_scenes)\n",
-    "# Print all images in specified Zoom range\n",
-    "for zoom in range (18,19):\n",
-    "    print_image(kokkai_scenes,number_scenes,zoom)\n",
-    "    \n",
-    "#X: 232836  Y: 103230  ZOOM: 18\n",
-    "#imageIndex \t:  8"
-   ]
-  }
- ],
- "metadata": {
-  "language_info": {
-   "codemirror_mode": {
-    "name": "ipython",
-    "version": 3
-   },
-   "file_extension": ".py",
-   "mimetype": "text/x-python",
-   "name": "python",
-   "nbconvert_exporter": "python",
-   "pygments_lexer": "ipython3",
-   "version": 3
-  },
-  "orig_nbformat": 2
- },
- "nbformat": 4,
- "nbformat_minor": 2
-}
+from asnaro import *
+
+# Tokyo station: 35.6812362,139.7649361
+a_scenes=get_ASNARO_scene(20.425278, 122.933611, 45.557222, 153.986389)
+
+a_scenes_length = len(a_scenes)
+print("Scenes length : ", a_scenes_length)
+
+#観察したい地域の緯度経度を指定する latitude & longitude 
+min_lat = 20.425278
+min_lon = 122.933611
+max_lat = 45.557222
+max_lon = 153.986389
+asnaro_scene = get_ASNARO_scene(min_lat, min_lon, max_lat, max_lon)
+#Filter scene in order
+asnaro_scenes = filter_by_date(asnaro_scene, 
+                               datetime(2014, 12, 7, tzinfo=timezone.utc),
+                               datetime(2019, 12, 31, tzinfo=timezone.utc))
+
+print("Asnaro Scenes: ", len(asnaro_scenes))
+
+#Specify your observation areas here : 
+observation_areas = [
+    {
+        "name":"東京駅",
+        "lat": 35.6812362,
+        "lon": 139.7649361
+    },
+    {
+        "name":"新宿駅",
+        "lat": 35.689607,
+        "lon": 139.700571
+    },
+    {
+        "name":"国会議事堂",
+        "lat": 35.675888,
+        "lon": 139.7426693
+    }
+]
+
+#Classify scenes by calculating distances and sort the acquisitionDate
+#Calculate the difference between lat&lot of the observation areas and the firstly defined area 
+classified_scenes = classify(observation_areas, asnaro_scenes)
+
+tokyostation_scenes = classified_scenes[0]
+shinjukustation_scenes = classified_scenes[1]
+kokkai_scenes = classified_scenes[2]
+print("東京駅-ASNARO: ",len(tokyostation_scenes))
+print("新宿駅-ASNARO:",len(shinjukustation_scenes))
+print("国会議事堂-ASNARO：",len(kokkai_scenes))
+
+# 以下東京駅表示にエラー。。。
+
+# Refer the Tile info from the below URL to get the specific zoom, xtile and ytile
+# https://maps.gsi.go.jp/development/tileCoordCheck.html#18/35.68131/139.76678
+
+#zoom=17
+#xtile=116419
+#ytile=51613
+#tokyostation_image = get_ASNARO_image(tokyostation_scenes[0]["entityId"], zoom, xtile, ytile)
+#print(tokyostation_image)
+#io.imshow(tokyostation_image)
+#plt.imshow(tokyostation_image)
+#plt.show()
+
+#number_scenes = len(tokyostation_scenes)
+
+# Print all images in specified Zoom range
+#for zoom in range (17,18):
+#    print(zoom)
+#    print_image(tokyostation_scenes, number_scenes, zoom)
+
+for scene in kokkai_scenes:
+    print(scene["acquisitionDate"])
+    
+
+#zoom=18
+#xtile=232839 #東京駅周辺x-18
+#ytile=103225 #東京駅周辺y-18
+
+zoom=18
+imgIndex=8
+xtile=232836 #国会議事堂 周辺？
+ytile=103230 #国会議事堂 周辺？
+
+#zoom=18
+#xtile=232829 #国会議事堂x-18
+#ytile=103231 #国会議事堂y-18
+
+#zoom=17 
+#xtile=116415 #国会議事堂x-17
+#ytile=51615 #国会議事堂y-17
+
+asnaro_image = get_ASNARO_image(kokkai_scenes[imgIndex]["entityId"], zoom, xtile, ytile)
+io.imshow(asnaro_image)
+
+# Get ASNARO image in series format
+def get_ASNARO_series_image(scene_id, zoom, topleft_x, topleft_y, size_x=1, size_y=1):
+    img = []
+    for y in range(size_y):
+        row = []
+        for x in range(size_x):
+            row.append(get_ASNARO_image(scene_id, zoom, topleft_x + x, topleft_y + y))
+        img.append(np.hstack(row))
+    return  np.vstack(img)
+
+zoom = 18
+imgIndex=8
+topleft_x = 232836 #国会議事堂 周辺？
+topleft_y = 103230 #国会議事堂 周辺？
+size_x = 10
+size_y = 10
+asnaro_series_image = get_ASNARO_series_image(kokkai_scenes[imgIndex]["entityId"], zoom, topleft_x, topleft_y, size_x, size_y)
+io.imshow(asnaro_series_image)
+
+# Get ASNARO image in grid display
+def make_grid_image(images, col):
+    img = []
+    for i in range(math.ceil(len(images)/col)):
+        row = []
+        for j in range(col):
+            index = i*col + j
+            if index < len(images):
+                row.append(images[index])
+            else:
+                row.append(np.ones_like(imgs[0])*255)
+        img.append(np.hstack(row))
+    return np.vstack(img)
+
+imgs = [get_ASNARO_series_image(scene["entityId"], zoom, topleft_x, topleft_y, size_x, size_y) for scene in kokkai_scenes]
+plt.rcParams["figure.figsize"] = (20, 20)
+io.imshow(make_grid_image(imgs, 5))
+
+
+# debug. Find xtile and ytile for specified zoom
+(xtile, ytile) = get_tile_num(35.675888, 139.7426693, zoom)
+print(xtile, ytile)
+
+# Zoom = 18 で表示できるxtileとytileを調べる
+
+number_scenes = len(kokkai_scenes)
+print(number_scenes)
+# Print all images in specified Zoom range
+for zoom in range (18,19):
+    print_image(kokkai_scenes,number_scenes,zoom)
+    
+#X: 232836  Y: 103230  ZOOM: 18
+#imageIndex 	:  8
