900b5d30bb9890b427151f844940005198d8e495
Dzulfahmi <fahmifahim@gmail.com>
2020-08-06 19:31:15 +0900

    Update sentinel.py

--- sentinel.py
+++ sentinel.py
@@ -1,3 +1,17 @@
+# IMPORTANT: Make sure you install below packages in advance
+#!pip install sentinelsat
+#!pip install rasterio
+#!apt install gdal-bin python-gdal python3-gdal 
+#!apt install python3-rtree 
+#!pip install git+git://github.com/geopandas/geopandas.git
+#!pip install descartes 
+#!pip install shapely
+#!pip install six
+#!pip install pyproj
+#!pip install descartes
+#!pip install geopandas
+#!pip install folium
+
 import os
 import numpy as np
 
