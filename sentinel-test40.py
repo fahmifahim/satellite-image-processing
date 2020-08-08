e18f79c1ce7b4741f9a192732074806607e235fa
Dzulfahmi <fahmifahim@gmail.com>
2020-08-08 15:29:22 +0900

    Create sentinel-test40.py

---
+++ sentinel-test40.py
@@ -0,0 +1,39 @@
+from sentinel2 import *
+
+# Get notebookname
+notebookbasename = os.path.basename(notebook_path())
+notebookname = os.path.splitext(notebookbasename)[0]
+print("JupyterNotebook name : ", notebookname)
+
+
+# FONT preparation setting. This font will be printed on each image
+# 1. Download the font file from https://osdn.net/dl/mplus-fonts/mplus-TESTFLIGHT-063a.tar.xz
+# 2. Execute thise command on your environment: wget https://osdn.net/dl/mplus-fonts/mplus-TESTFLIGHT-063a.tar.xz
+# 3. Check the downloaded file: ls -l mplus-TESTFLIGHT-063a.tar.xz
+# 4. -rw-r--r-- 1 jovyan users 10371708 Apr 23  2019 mplus-TESTFLIGHT-063a.tar.xz
+# 5. Put the downloaded file to the same directory as your JupyterNotebook file
+
+cwd = os.getcwd()
+suffix = '.ttf'
+base_filename = 'mplus-1c-bold'
+fontfile = os.path.join(cwd,'mplus-TESTFLIGHT-063a',base_filename + suffix)
+
+# Extract the font file. You may skip the process if it already there
+if os.path.isfile(fontfile):
+    print("Font file exist : " + fontfile)
+else: 
+    print("Extracting font file...")
+    
+    if os.path.isfile("mplus-TESTFLIGHT-063a.tar.xz"):
+        !xz -dc mplus-TESTFLIGHT-*.tar.xz | tar xf -
+    else:
+        print("Font file extraction failed!")
+        
+    print("Font file extracted")
+    if os.path.isfile(fontfile):
+        print(fontfile)
+
+#Determine your Area of Interest by using the 
+#IMPORTANT: It is currently commented out. Use Polyline Tool from Keene University to find out the area of interest
+#from IPython.display import HTML
+#HTML(r'<iframe width="960" height="480" src="https://www.keene.edu/campus/maps/tool/" frameborder="0"></iframe>')
