b45f6831f1939f0471c50de7d8641abaf208b492
Dzulfahmi <fahmifahim@gmail.com>
2020-08-13 14:33:06 +0900

    Update sentinel-test60-tokyo.py

--- sentinel-test60-tokyo.py
+++ sentinel-test60-tokyo.py
@@ -32,3 +32,93 @@ else:
     print("Font file extracted")
     if os.path.isfile(fontfile):
         print(fontfile)
+#Determine your Area of Interest by using the 
+#IMPORTANT: It is currently commented out. Use Polyline Tool from Keene University to find out the area of interest
+#from IPython.display import HTML
+#HTML(r'<iframe width="960" height="480" src="https://www.keene.edu/campus/maps/tool/" frameborder="0"></iframe>')
+
+#Copy paste the area of interest from the Polyline Tool Keene University 
+#Area name = Tokyo 
+AREA = [
+          [
+            -220.291841,
+            35.6593884
+          ],
+          [
+            -220.2932143,
+            35.4817801
+          ],
+          [
+            -220.1380324,
+            35.4817801
+          ],
+          [
+            -220.1421523,
+            35.6493456
+          ],
+          [
+            -220.291841,
+            35.6593884
+          ]
+        ]
+
+#Set the Resolution to display. Choose between 10, 20, or 60
+resolution = "10"
+print("Resolution to retrieve from Sentinel-2 = ", resolution)
+
+
+# Convert coordinate to 360degree format
+for i in range(len(AREA)):
+    AREA[i][0] = AREA[i][0] +360
+
+# Convert coordinate to Polygon format
+m=Polygon([AREA])
+
+#Set the object name for the area of interest
+object_name = str(notebookname)
+
+# Convert the Polygon data to GeoJSON format
+Sentinel2_convert_polygon_to_json(object_name, m)
+
+# Convert a GeoJSON object to Well-Known Text. Intended for use with OpenSearch queries.
+footprint_geojson = geojson_to_wkt(read_geojson(object_name +'.geojson'))
+print(footprint_geojson)
+
+
+#Get the Sentinel-2 L2A products from the specified starting date
+#Specify your desired starting and ending date
+start_date = '20200601'
+end_date = '20200701'
+
+#Define the index. Set as 0 if you want to get the first index
+index = 0
+
+#Count the number of months from different years
+num_months = int(end_date[2:4]) - int(start_date[2:4])
+num_months = num_months*12 + int(end_date[4:6]) - int(start_date[4:6])
+
+#Loop process to get all datas
+for i in range(num_months):
+    if i < 1:
+        m = int(start_date[4:6])
+    else:
+        m = int(start_date[4:6]) +1
+
+    if m <10:
+        start_date = start_date[:4] +'0'+ str(m) + start_date[6:]
+    elif m <13:
+        start_date = start_date[:4] + str(m) + start_date[6:]
+    else:
+        y = int(start_date[2:4]) +1
+        start_date = start_date[:2] + str(y) + '01' + start_date[6:]
+        
+    end_date = start_date[:6] +'28'
+    
+    #Get Sentinel-2 GeoDataFrame
+    products_gdf = Sentinel2_get_productsgdf(footprint_geojson, start_date, end_date)
+    number_scenes = len(products_gdf)
+    print("Number of scenes: ", number_scenes)
+    
+    # Call the Sentinel function
+    for index in range(number_scenes):
+        Sentinel2_get_all_data(products_gdf, index, fontfile, object_name, start_date, resolution)
