359c82345ac673da6bd3042660de67cab07744f9
Dzulfahmi <fahmifahim@gmail.com>
2020-11-05 08:01:11 +0900

    Create srcnn_predict.py

---
+++ srcnn/srcnn_predict.py
@@ -0,0 +1,65 @@
+import os
+
+import time
+import matplotlib.pyplot as plt
+import matplotlib
+matplotlib.use('Agg')
+
+if True:
+    import keras
+
+    from srcnn_asnaro1_train \
+        import dataset_dir, n_test, epochs, batch_size, psnr, data_generator
+
+if __name__ == '__main__':
+
+    #N_show = 319  # N_show番目の評価用データを表示
+    N_show = 1  # N_show番目の評価用データを表示
+
+    model_filename = 'srcnn_asnaro1_train' + \
+        '-ep' + str(epochs) + \
+        '-bs' + str(batch_size)
+    path_to_model = './data/' + model_filename + '.h5'
+    result_dir = 'rslt/'
+
+    if not os.path.exists(result_dir):
+        os.mkdir(result_dir)
+
+    # Time stamp
+    timestr = time.strftime("%Y%m%d-%H%M%S")
+
+    test_x, test_y = \
+        next(data_generator(dataset_dir, 'test', scale=4.0,
+                            batch_size=n_test, shuffle=False))
+
+    # Reload a pretrained Keras model from the saved model:
+    # model = keras.models.load_model(path_to_model, custom_objects=[psnr])
+    model = keras.models.load_model(path_to_model,
+                                    custom_objects={'psnr': psnr})
+
+    # Check its architecture
+    model.summary()
+
+    # Do the prediction
+    pred = model.predict(test_x)
+
+    fig = plt.figure(figsize=(50, 50), facecolor="w")
+
+    plt.subplot(1, 3, 1)
+    plt.title("Original", fontsize=80)
+    plt.tight_layout()
+    plt.imshow(test_y[N_show, :, :])
+
+    plt.subplot(1, 3, 2)
+    plt.title("Low resolution", fontsize=80)
+    plt.tight_layout()
+    plt.imshow(test_x[N_show, :, :])
+
+    plt.subplot(1, 3, 3)
+    plt.title("SRCNN result", fontsize=80)
+    plt.tight_layout()
+    plt.imshow(pred[N_show, :, :])
+
+    print('Saving SRCNN result image...')
+    plt.savefig(result_dir + model_filename +
+                '-' + str(N_show) + '-' + timestr + '.png')
