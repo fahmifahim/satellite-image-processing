20a1b38d40388910ef92b6134b3a4b22a731d106
fahmifahim <fahmifahim@gmail.com>
2020-11-14 14:16:38 +0900

    Update image_resize_debug.py

--- srcnn/image_resize_debug.py
+++ srcnn/image_resize_debug.py
@@ -1,13 +1,3 @@
-from PIL import Image
-
-#im = Image.open('original.png')
-im = Image.open('Data_ASNARO1_SRCNN/train/output_z18_103230_232831.png')
-print(im)
-
-im_resize = im.resize(size=(64,64))
-print(im_resize)
-
-
 import numpy as np
 from PIL import Image
 
