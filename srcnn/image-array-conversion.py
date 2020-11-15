9406526d6b3094c154e8d98d4ee6f678e128fb1d
fahmifahim <fahmifahim@gmail.com>
2020-11-16 07:41:33 +0900

    Update image-array-conversion.py

--- srcnn/image-array-conversion.py
+++ srcnn/image-array-conversion.py
@@ -8,9 +8,21 @@ im2 = load_img(im_path, grayscale=False, color_mode='rgb', target_size=(256,256)
 im3 = load_img(im_path, grayscale=False, color_mode='rgba', target_size=(256,256))
 
 
-print("---Original")
-print(im2)
-im_sequence = im2.getdata()
-#im_array = np.array(im_sequence)
-im_array = img_to_array(im_sequence)
+print("---PIL")
+print(im1)
+im_sequence = im1.getdata()
+im_array = np.array(im_sequence)
 print(im_array)
+
+#new_im1 = Image.fromarray(im_array)
+new_im1 = Image.fromarray(np.uint8(im_array))
+print(new_im1)
+
+print("\n\n---KERAS")
+print(im2)
+im3_sequence = im3.getdata()
+im3_array = img_to_array(im3_sequence)
+print(im3_array)
+
+new_im3 = array_to_img(im3_array)
+print(new_im3)
