a75972db96ce87808a8631e0461f1c672cf6bfc9
fahmifahim <fahmifahim@gmail.com>
2020-11-16 12:59:54 +0900

    Update image-array-rescale.py

--- srcnn/image-array-rescale.py
+++ srcnn/image-array-rescale.py
@@ -1,6 +1,7 @@
 import numpy as np
 from PIL import Image
 from skimage.transform import resize, rescale, downscale_local_mean
+from skimage.io import imsave, imread
 from keras.preprocessing.image import img_to_array, load_img, array_to_img
 
 im_path = 'Data_ASNARO1_SRCNN/train/output_z18_103230_232831.png'
@@ -18,9 +19,11 @@ im_sequence = im1.getdata()
 im_array = np.array(im_sequence)
 print(im_array)
 
-#print("\nRESIZE ARRAY to 64")
-#im_resize64 = resize(im_array,(64,64))
-#print(im_resize64)
+
+print("\nRESCALE ARRAY to 64 scale factor 0.25")
+im2_rescale64 = rescale(im_array, 0.25, anti_aliasing=False)
+print(im2_rescale64)
+
 
 print("\n\n---KERAS")
 print(im2)
@@ -32,6 +35,14 @@ print("\nRESIZE ARRAY to 64")
 im2_resize64 = resize(im2_array, (64, 64), anti_aliasing=True)
 print(im2_resize64)
 
-print("\nRESCALE ARRAY to 64 scale factor 0.25")
-im2_rescale64 = rescale(im2_array, 0.25, anti_aliasing=False)
-print(im2_rescale64)
+#img_im2_rescale64 = array_to_img(im2_rescale64)
+img_im2_rescale64 = np.array(im2_resize64, dtype=np.uint8)
+imsave("test.png", img_im2_rescale64)
+print("\nimg_im2_rescale64:")
+print(img_im2_rescale64)
+print(array_to_img(img_im2_rescale64))
+
+
+#print("\nRESCALE ARRAY to 64 scale factor 0.25")
+#im2_rescale64 = rescale(im2_array, 0.25, anti_aliasing=False)
+#print(im2_rescale64)
